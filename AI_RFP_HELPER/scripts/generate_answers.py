1144c5bb137312704cc101afd7a8243915c7a77b
Jonathan Davis <jonathandavis@jonathans-mbp.attlocal.net>
2023-07-20 21:33:50 -0500

    modify script

--- AI_RFP_HELPER/scripts/generate_answers.py
+++ AI_RFP_HELPER/scripts/generate_answers.py
@@ -5,20 +5,16 @@ def generate(response):
     response = json.loads(response)
     content = response['choices'][0]['message']['content']
 
-    # split content into lines
-    lines = content.split('\n\n')
+    # split content into answers
+    answers = content.split('Answer to Question')[1:]  # splitting by 'Answer to Question' and skipping the first element to exclude any leading text
 
-    # separate questions and answers
-    qa_pairs = []
-    for i in range(0, len(lines), 2):
-        try:
-            qa_pairs.append((lines[i], lines[i + 1]))
-        except IndexError:  # handles the case where there is not an even number of lines
-            qa_pairs.append((lines[i], ''))  # uses an empty string as the answer if there is no pair
+    # clean up answers (remove numbers and extra spaces)
+    answers = [answer.split(': ')[1].strip() for answer in answers]
 
     # write to CSV
     with open('answers.csv', 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["Question", "Answer"])
-        writer.writerows(qa_pairs)
+        writer.writerow(["Answer"])  # header
+        for answer in answers:
+            writer.writerow([answer])
 
